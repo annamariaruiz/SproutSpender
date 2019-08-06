f2fe8e695fd5ca326e6d9ce8e7f613651aaae78f
Anna Ruiz <45431125+lilhappyburrito@users.noreply.github.com>
2019-08-06 09:52:46 -0600

    Home

--- src/main/java/pro100/group10/sproutspender/controllers/HomeController.java
+++ src/main/java/pro100/group10/sproutspender/controllers/HomeController.java
@@ -2,13 +2,28 @@ package pro100.group10.sproutspender.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HomeController {
 	
 	@FXML
 	private Button start;
 	
+	@FXML
+	private TextField dbName;
+	
+	@FXML
+	private Label alert;
+	
 	public void init() {
-		System.out.println("Home Controller");
+		if(dbName.getText().trim().isEmpty()) {
+			alert.setText("Database name must not be empty");
+			alert.setLayoutX(213);
+		} else {
+			alert.setText("");
+		}
+		
+		
 	}
 }
