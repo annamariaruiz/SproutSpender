c912e4d01f1e383a0b6becd25048140898a63e7f
Anna Ruiz <45431125+lilhappyburrito@users.noreply.github.com>
2019-08-06 09:20:43 -0600

    Start window

--- src/main/java/pro100/group10/sproutspender/app/Main.java
+++ src/main/java/pro100/group10/sproutspender/app/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Home.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("../views/Home.fxml"));
+			Scene scene = new Scene(root,600,400);
+			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/main/java/pro100/group10/sproutspender/controllers/HomeController.java
+++ src/main/java/pro100/group10/sproutspender/controllers/HomeController.java
@@ -1,5 +1,14 @@
 package pro100.group10.sproutspender.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HomeController {
 	
+	@FXML
+	private Button start;
+	
+	public void init() {
+		System.out.println("Home Controller");
+	}
 }
