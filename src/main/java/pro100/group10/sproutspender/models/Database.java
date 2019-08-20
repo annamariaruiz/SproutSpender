eee5c56ea194dce4372033114e317e162f6062c9
Anna Ruiz <45431125+lilhappyburrito@users.noreply.github.com>
2019-08-20 07:55:39 -0600

    Update Database.java

--- src/main/java/pro100/group10/sproutspender/models/Database.java
+++ src/main/java/pro100/group10/sproutspender/models/Database.java
@@ -302,7 +302,7 @@ public class Database {
 				String dateS = rs.getString("endDate");
 				Date date = Date.valueOf(dateS);
 				
-				String category = rs.getString("CategoryType");
+				String category = rs.getString("category");
 				CategoryType cat = Budget.CategoryType.GENERAL;
 				if(("food").equalsIgnoreCase(category)) {
 					cat = Budget.CategoryType.FOOD;
